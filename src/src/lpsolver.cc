baab82bfb27cd8a729530ecfaedc71fdf622936b
Mingfu Shao <shaomingfu@gmail.com>
2016-01-17 01:30:47 -0500

    libgurobi65.so is necessary

--- src/src/lpsolver.cc
+++ src/src/lpsolver.cc
@@ -2,10 +2,16 @@
 
 lpsolver::lpsolver(const bbase &bb)
 	: sgraph(bb)
-{}
+{
+	env = new GRBEnv();
+	model = new GRBModel(*env);
+}
 
 lpsolver::~lpsolver()
-{}
+{
+	delete model;
+	delete env;
+}
 
 int lpsolver::solve()
 {
