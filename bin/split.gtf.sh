d131ed5ccb0c6b90e99cbda8c7c920972bbca60e
Mingfu Shao <shaomingfu@gmail.com>
2016-02-17 17:14:09 -0500

    update

--- bin/split.gtf.sh
+++ bin/split.gtf.sh
@@ -15,5 +15,5 @@ cat $file | grep "gene_id \"$1\"" | grep -v abundance | grep -v FPKM > $tmp
 
 for i in `cat $tmp | cut -f 9 | cut -f 4 -d " " | cut -f 2 -d "\"" | sort | uniq`
 do
-	cat $tmp | grep "transcript_id \"$i\"" > $2/$i
+	cat $tmp | grep "transcript_id \"$i\"" > $2/"$i".gtf
 done
