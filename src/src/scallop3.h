6c11a36825fda1dcc9cf6b2889f9941ade88544f
Mingfu Shao <shaomingfu@gmail.com>
2016-02-29 01:40:06 -0500

    add scallop3

---
+++ src/src/scallop3.h
@@ -0,0 +1,29 @@
+#ifndef __SCALLOP3_H__
+#define __SCALLOP3_H__
+
+#include "assembler.h"
+
+typedef map< edge_descriptor, vector<int> > MEV;
+typedef pair< edge_descriptor, vector<int> > PEV;
+
+// dynamic programming for nested graph
+class scallop3 : public assembler
+{
+public:
+	scallop3(splice_graph &gr);
+	virtual ~scallop3();
+
+public:
+	MEV mev;
+
+public:
+	int assemble();
+
+private:
+	int print();
+	int init_super_edges();
+	int reconstruct_splice_graph();
+	bool decompose_trivial_vertex(int x);
+};
+
+#endif
