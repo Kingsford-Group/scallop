ef997d576615a56b025b66cc8cba991dc4c12246
Mingfu Shao <shaomingfu@gmail.com>
2016-03-17 22:38:45 -0400

    update create.pdf.sh

--- bin/create.pdf.sh
+++ bin/create.pdf.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-prefix="ABI3BP.gr"
-output="ABI3BP.pdf"
+num=18
+gene="ABI3BP"
+
+
+prefix="$gene".gr
+output="$gene".pdf
 
 rm -rf $output
 
-for i in `seq 0 18`
+ss=""
+for i in `seq 0 $num`
 do
-	myepstool.sh "$prefix"."$i"
 	s="$prefix"."$i".pdf
 	ss="$ss $s"
+	echo $s
+	myepstool.sh "$prefix"."$i" 1> /dev/null 2>/dev/null
+done
+
+pdftk $ss cat output $output
+rm "$prefix".*
+
+
+prefix="$gene".nt
+output="$gene".x.pdf
+
+rm -rf $output
+
+ss=""
+for i in `seq 0 $num`
+do
+	s="$prefix"."$i".pdf
+	ss="$ss $s"
+	echo $s
+	myepstool.sh "$prefix"."$i" 1> /dev/null 2>/dev/null
 done
 
 pdftk $ss cat output $output
