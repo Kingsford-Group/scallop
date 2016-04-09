42e887cb550b2ce436e1cb1404ef56173b27f1cd
Mingfu Shao <shaomingfu@gmail.com>
2016-04-09 13:49:35 -0400

    add util to src

--- src/src/config.h
+++ src/src/config.h
@@ -1,22 +1,12 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+#include "util.h"
 #include <stdint.h>
 #include <map>
 #include <sstream>
 
 using namespace std;
-
-// macros: using int64_t for two int32_t
-#define pack(x, y) (int64_t)((((int64_t)(x)) << 32) | ((int64_t)(y)))
-#define high32(x) (int32_t)((x) >> 32)
-#define low32(x) (int32_t)(((x) << 32) >> 32)
-
-
-// definitions
-typedef map<int32_t, int> MPI;
-typedef pair<int32_t, int> PPI;
-
 
 // constants
 #define LEFT_SPLICE 1
@@ -63,14 +53,5 @@ bool parse_arguments(int argc, const char ** argv);
 
 // load parameters
 int load_config(const char * conf_file);
-
-// common small functions
-template<typename T>
-string tostring(T t)
-{
-	ostringstream s;
-	s << t;
-	return s.str();
-}
 
 #endif
