f048152452d989dc2350bbbc1d7cbac08b558d4b
Mingfu Shao <shaomingfu@gmail.com>
2016-06-14 13:49:00 -0400

    add make.exp.sim.sh

--- bin/make.exp.sim.sh
+++ bin/make.exp.sim.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-num=10
-dir=./simulation1
-params=flux.params
-gtf=GSM981256/genes1.gtf
+dir=./ensembl/mouse/
 
-for i in `seq 1 $num`
-do
-	echo "simulate $i"
-#flux-simulator -p $params -x
-	cur=$dir/$i
-#mkdir -p $cur
-#mv profile $cur
-	./merge.expression.pl $gtf $cur/profile > $cur/expression.gtf
-done
+#./purify.gtf.pl $dir/p0.gtf > $dir/p1.gtf
+#./fix.gtf.sh $dir/p1.gtf > $dir/p2.gtf
+
+params=flux.exp.params
+flux-simulator -p $params -x
+mv profile $dir
+
+gtf=$dir/p2.gtf
+./merge.exp.pl $gtf $dir/profile > $dir/expression.gtf
+
+./scallop -c ./config -i $dir/expression.gtf -o $dir/s1.gtf -a core -s 5 > $dir/log1
+./scallop -c ./config -i $dir/expression.gtf -o $dir/s2.gtf -a full -s 5 > $dir/log2
+./scallop -c ./config -i $dir/expression.gtf -o $dir/s3.gtf -a greedy -s 5 > $dir/log3
