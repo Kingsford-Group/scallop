5f30bf1e1d1c73fe7db5f6ff92d52edae60c149c
Mingfu Shao <shaomingfu@gmail.com>
2016-06-26 02:40:52 -0400

    update

--- bin/run.exp.sim.sh
+++ bin/run.exp.sim.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-num=10
-dir=./simulation1
+num=100
+dir=./human/exp1
 
 for i in `seq 1 $num`
 do
 	echo "run simulation $i"
 	cur=$dir/$i
 
-	./scallop -c ./config -i $cur/expression.gtf -o $cur/refseq.gtf > $cur/refseq.log
-#./scallop -c ./config -i $cur/expression.gtf -o $cur/scallop0.gtf -a scallop0 > $cur/scallop0.log
-#./scallop -c ./config -i $cur/expression.gtf -o $cur/scallop1.gtf -a scallop1 > $cur/scallop1.log
-#./scallop -c ./config -i $cur/expression.gtf -o $cur/scallop2.gtf -a scallop2 > $cur/scallop2.log
-#./scallop -c ./config -i $cur/expression.gtf -o $cur/greedy.gtf -a greedy > $cur/greedy.log
-
+	./scallop -c ./config -i $cur/expression.gtf -o $cur/scallop1.gtf -a core -s 5 > $cur/scallop1.log
+	./scallop -c ./config -i $cur/expression.gtf -o $cur/scallop2.gtf -a full -s 5 > $cur/scallop2.log
+	./scallop -c ./config -i $cur/expression.gtf -o $cur/scallop3.gtf -a greedy -s 5 > $cur/scallop3.log
+	./gtfcompare $cur/scallop1.gtf $cur/expression.gtf > $cur/scallop1.cmp
+	./gtfcompare $cur/scallop2.gtf $cur/expression.gtf > $cur/scallop2.cmp
+	./gtfcompare $cur/scallop3.gtf $cur/expression.gtf > $cur/scallop3.cmp
 done
