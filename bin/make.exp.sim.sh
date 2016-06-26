f495bf73d16359fae3ae3c1b94dbd553e7626b79
Mingfu Shao <shaomingfu@gmail.com>
2016-06-26 02:37:53 -0400

    update

--- bin/make.exp.sim.sh
+++ bin/make.exp.sim.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
-dir=./ensembl/zebrafish2
+name=genes1_sorted.gtf
+gtf=`pwd`/$name
+dir=`pwd`/human/exp1
+num=100
+
+mkdir -p $dir
 
 #./purify.gtf.pl $dir/p0.gtf > $dir/p1.gtf
 #./fix.gtf.sh $dir/p1.gtf > $dir/p2.gtf
 
-param0=flux.params.template
-params=$dir/params
+for i in `seq 1 $num`
+do
+	cur=$dir/$i
+	mkdir -p $cur
+	ln -sf $gtf $cur/
+	params=$cur/params
 
-cp $param0 $params
-echo "REF_FILE_NAME	p2.gtf" >> $params
-echo "NB_MOLECULES	5000000" >> $params
-echo "EXPRESSION_K	-0.1" >> $params
+	echo "REF_FILE_NAME	$name" > $params
+	echo "NB_MOLECULES	5000000" >> $params
+	echo "EXPRESSION_K	-0.1" >> $params
+	echo "GEN_DIR			." >> $params
+	echo "LOAD_CODING		true" >> $params
+	echo "LOAD_NONCODING	false" >> $params
+	echo "PRO_FILE_NAME	profile" >> $params
+	echo "LIB_FILE_NAME	libfile" >> $params
 
-flux-simulator -p $params -x
-#mv profile $dir
-
-gtf=$dir/p2.gtf
-./merge.exp.pl $gtf $dir/profile > $dir/expression.gtf
-
-./scallop -c ./config -i $dir/expression.gtf -o $dir/s1.gtf -a core -s 5 > $dir/log1
-./scallop -c ./config -i $dir/expression.gtf -o $dir/s2.gtf -a full -s 5 > $dir/log2
-./scallop -c ./config -i $dir/expression.gtf -o $dir/s3.gtf -a greedy -s 5 > $dir/log3
-
-./gtfcompare $dir/s1.gtf $dir/expression.gtf > $dir/s1.cmp
-./gtfcompare $dir/s2.gtf $dir/expression.gtf > $dir/s2.cmp
-./gtfcompare $dir/s3.gtf $dir/expression.gtf > $dir/s3.cmp
-
-./gtfformat $dir/expression.gtf $dir/expression2.gtf
+	flux-simulator -p $params -x
+	
+	./merge.exp.pl $gtf $cur/profile > $cur/expression.gtf
+	
+#	./scallop -c ./config -i $dir/expression.gtf -o $dir/s1.gtf -a core -s 5 > $dir/log1
+#	./scallop -c ./config -i $dir/expression.gtf -o $dir/s2.gtf -a full -s 5 > $dir/log2
+#	./scallop -c ./config -i $dir/expression.gtf -o $dir/s3.gtf -a greedy -s 5 > $dir/log3
+#	
+#	./gtfcompare $dir/s1.gtf $dir/expression.gtf > $dir/s1.cmp
+#	./gtfcompare $dir/s2.gtf $dir/expression.gtf > $dir/s2.cmp
+#	./gtfcompare $dir/s3.gtf $dir/expression.gtf > $dir/s3.cmp
+#	
+#	./gtfformat $dir/expression.gtf $dir/expression2.gtf
+done
