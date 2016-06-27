060f8fb879c62c52bc210ba5f10b0dd1eb8d59d0
Mingfu Shao <shaomingfu@gmail.com>
2016-06-26 20:20:25 -0400

    update purify.gtf.pl for ensembl

--- bin/purify.gtf.pl
+++ bin/purify.gtf.pl
@@ -5,28 +5,35 @@ if($#ARGV != 0)
 	die("usage: ./compare.gtf.pl <gtf-file>\n");
 }
 
+my $chr = "";
+
 open(FILE, '<', $ARGV[0]) or die("open gtf file $ARGV[0] error\n");
 while(<FILE>)
 {
 	chomp;
 	my @line = split('\t');
+
+	if($line[2] eq "gene")
+	{
+		next;
+	}
 		
 	$f = 0;
 	for($k = 1; $k <= 22; $k++)
 	{
-		if($line[0] eq "chr$k")
+		if($line[0] eq "$chr"."$k")
 		{
 			$f = 1;
 			last;
 		}
 	}
 
-	if($line[0] eq "chrX")
+	if($line[0] eq "$chr"."X")
 	{
 		$f = 1;
 	}
 
-	if($line[0] eq "chrY")
+	if($line[0] eq "$chr"."Y")
 	{
 		$f = 1;
 	}
