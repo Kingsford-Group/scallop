168c800914a21a563dd099514e58ce3124883b5b
Mingfu Shao <shaomingfu@gmail.com>
2016-07-18 00:50:33 -0400

    update scripts

--- bin2/run.case.sh
+++ bin2/run.case.sh
@@ -9,15 +9,20 @@ fi
 mkdir -p tex
 ln -sf ../llncs.cls tex/
 
-./scallop -i ./expression.gtf -g $1 -a full -o $1.x.gtf -t > $1.x.log 
+ln -sf ../bam/$1.bam .
+samtools index $1.bam
 
+stringtie $1.bam -o $1.st.gtf
+cuffcompare -r ../gtf/$1.gtf $1.st.gtf
+
+../../scallop -i $1.bam -a full -o $1.s1.gtf -t > $1.s1.log
+mkdir -p tex
+rm -rf tex/bundle.0.*.tex
+mv bundle.0.*.tex tex
+./create.pdf.sh bundle.0
+
+../../scallop -i ../gtf/$1.gtf -a full -o $1.s2.gtf -t > $1.s2.log
 mkdir -p tex
 rm -rf tex/$1.*.tex
 mv $1.*.tex tex
 ./create.pdf.sh $1
-
-
-./scallop -i ./genes/$1.sort.bam -a full -o $1.y.gtf -t > $1.y.log 
-rm -rf tex/bundle.0.*.tex
-mv bundle.0.*.tex tex
-./create.pdf.sh bundle.0
