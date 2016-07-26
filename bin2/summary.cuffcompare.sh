0d2b6287d47c2021ab1bcef71487fadcdfb05a71
Mingfu Shao <shaomingfu@gmail.com>
2016-07-26 19:27:27 -0400

    add summary scripts

--- bin2/summary.cuffcompare.sh
+++ bin2/summary.cuffcompare.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-IFS="
-"
+tmp0=/tmp/tmp0
+tmp1=/tmp/tmp1
+tmp2=/tmp/tmp2
+tmp3=/tmp/tmp3
+tmp4=/tmp/tmp4
+tmp5=/tmp/tmp5
 
-for i in `ls $1 | grep -v gtf | grep -v log | grep -v track | grep -v loci`
-do
-	x1=`cat $1/$i | grep mRNA | grep Query | cut -c 26-27`
-	x2=`cat $1/$i | grep mRNA | grep Reference | cut -c 26-27`
-	x3=`cat $1/$i | grep chain | grep Intron | cut -c 22-26`
-	x4=`cat $1/$i | grep chain | grep Intron | cut -c 28-32`
+ls $1 | grep -v gtf | grep -v log | grep -v track | grep -v loci > $tmp0
 
-	echo $i $x1 $x2 $x3 $x4
-done
+s=`cat $tmp0`
+
+cd $1
+
+cat $s | grep mRNA | grep Query | cut -c 26-27 > $tmp1
+cat $s | grep mRNA | grep Reference | cut -c 26-27 > $tmp2
+
+paste $tmp0 $tmp1 $tmp2 | awk '$2 != 0' > $tmp3
+paste $tmp0 $tmp1 $tmp2 | awk '$2 == 0' > $tmp4
+
+cat $s | grep chain | grep Intron | cut -c 22-26 > $tmp1
+cat $s | grep chain | grep Intron | cut -c 28-32 > $tmp2
+
+paste $tmp3 $tmp1 $tmp2
+paste $tmp4 $tmp4 $tmp4 | cut -f 1,2,3,5,8
