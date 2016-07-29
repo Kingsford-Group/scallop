be6131ecbe031527fa35f689b67540794990f3c8
Mingfu Shao <shaomingfu@gmail.com>
2016-07-29 19:32:30 -0400

    update scripts

--- bin2/evaluate.09.sh
+++ bin2/evaluate.09.sh
@@ -16,6 +16,6 @@ do
 	b1=`../gtfcompare $ref/$i.normal.gtf $st/$i.normal.gtf 2 | cut -f 3,6,9,12,15 -d " "`
 	b2=`../gtfcompare $ref/$i.trivial.gtf $st/$i.trivial.gtf 2 | cut -f 3,6,9,12,15 -d " "`
 
-	echo "$i TRIVIAL $a1 $b1" | awk '{print $1, $2, $3, $4, $9, $5, $10, $6, $11, $7, $12}'
-	echo "$i NORMAL $a2 $b2" | awk '{print $1, $2, $3, $4, $9, $5, $10, $6, $11, $7, $12}'
+	echo "$i NORMAL $a1 $b1" | awk '{print $1, $2, $3, $4, $9, $5, $10, $6, $11, $7, $12}'
+	echo "$i TRIVIAL $a2 $b2" | awk '{print $1, $2, $3, $4, $9, $5, $10, $6, $11, $7, $12}'
 done
