f697cf8161eec258aaad0ca9dcf28bc2bf0791e4
Mingfu Shao <shaomingfu@gmail.com>
2016-07-29 19:22:27 -0400

    update scripts

--- bin2/classify.reference.sh
+++ bin2/classify.reference.sh
@@ -2,23 +2,30 @@
 
 IFS="
 "
-ref=./reference09
+ref=./reference9
 gtf=./gtf
 
-mkdir -p $ref
-rm -rf $ref/*.gtf
+#mkdir -p $ref
+#rm -rf $ref/*.gtf
 
 for i in `cat genes.list`
 do
 	a=`echo "$i" | cut -f 1 -d " "`
 	b=`echo "$i" | cut -f 2 -d " "`
+	c=`echo "$i" | cut -f 3 -d " "`
 
 	if [ "$b" -ge 10 ]
 	then
 		continue
 	fi
 
-	echo $a $b
+	echo $a $b $c
 
-	cat $gtf/$a.gtf >> $ref/$b.gtf
+	if [ "$c" == "TRIVIAL" ]
+	then
+		cat $gtf/$a.gtf >> $ref/$b.trivial.gtf
+	elif [ "$c" == "NORMAL" ]
+	then
+		cat $gtf/$a.gtf >> $ref/$b.normal.gtf
+	fi
 done
--- bin2/collect.gtf.sh
+++ bin2/collect.gtf.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ];
+then
+	echo "usage $0 directory full/STRG"
+	exit
+fi
+
 script=./scritps.sh
 rm -rf $script
 
@@ -9,7 +15,7 @@ chmod +x $script
 for i in `ls $1 | grep gtf`
 do
 	k=${i/.gtf/}
-	echo "cat $1/$i | sed 's/STRG\./$k./g'" >> $script
+	echo "cat $1/$i | sed 's/$2\./$k./g'" >> $script
 done
 
 ./$script
