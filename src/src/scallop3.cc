9013363e32a2110b6facf21b0ca83d9843f8e1a5
Mingfu Shao <shaomingfu@gmail.com>
2016-09-25 00:32:22 -0400

    use 999 in resolve_nontrivial_vertex for smallest edge ratio

--- src/src/scallop3.cc
+++ src/src/scallop3.cc
@@ -155,7 +155,8 @@ bool scallop3::resolve_nontrivial_vertex(bool split, bool hyper)
 
 	int se;
 	//double ratio2 = compute_smallest_edge(root, se);
-	double ratio2 = compute_smallest_removable_edge(se);
+	//double ratio2 = compute_smallest_removable_edge(se);
+	double ratio2 = 999;
 
 	double ratio = (ratio1 < ratio2) ? ratio1 : ratio2;
 	if(ratio > max_split_error_ratio) return false;
