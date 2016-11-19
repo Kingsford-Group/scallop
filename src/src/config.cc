13c8c86e3aa2d249a72cb1e57707170c4e81c8cc
Mingfu Shao <mingfu.shao@cs.cmu.edu>
2016-11-19 11:36:55 -0500

    change back to B310, change default gap = 15

--- src/src/config.cc
+++ src/src/config.cc
@@ -18,13 +18,13 @@ int32_t min_splice_boundary_hits = 1;
 
 // for identifying subgraphs
 double max_indel_ratio = 0.2;
-int32_t min_subregion_gap = 3;
+int32_t min_subregion_gap = 15;
 int32_t min_subregion_length = 15;
 double min_subregion_overlap = 2;
 int min_subregion_ladders = 0;
 
 // for splice graph
-double min_inner_vertex_weight = 5.0;
+double min_inner_vertex_weight = 10;
 double min_inner_boundary_weight = 4.0;
 double min_splice_edge_weight = 3.5;
 double min_spanning_edge_weight = 5.5;
