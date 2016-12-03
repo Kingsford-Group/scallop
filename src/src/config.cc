760284e75dabe744a36d5a7f1e346781f47bded0
Mingfu Shao <mingfu.shao@cs.cmu.edu>
2016-12-03 18:06:46 -0500

    change default of subregion coverage to 1.5

--- src/src/config.cc
+++ src/src/config.cc
@@ -20,8 +20,8 @@ int library_type = UNSTRAND;
 
 // for identifying subgraphs
 double max_indel_ratio = 0.2;
-double min_subregion_overlap = 2;
 int32_t min_subregion_gap = 3;
+double min_subregion_overlap = 1.5;
 int32_t min_subregion_length = 15;
 
 // for revising/decomposing splice graph
