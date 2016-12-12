6c2f0a5ae64db6217449262f395e39c8d324b6c7
Mingfu Shao <mingfu.shao@cs.cmu.edu>
2016-12-11 21:57:31 -0500

    split h and cc

--- lib/util/src/util.cc
+++ lib/util/src/util.cc
@@ -1,1 +1,17 @@
 #include "util.h"
+
+vector<int> get_random_permutation(int n)
+{
+	vector<int> v;
+	for(int i = 0; i < n; i++) v.push_back(i);
+	for(int i = 0; i < n; i++)
+	{
+		int k = rand() % (n - i);
+		int x = v[k];
+		v[k] = v[n - i - 1];
+		v[n - i - 1] = x;
+	}
+	return v;
+}
+
+
--- lib/util/src/util.h
+++ lib/util/src/util.h
@@ -162,18 +162,6 @@ vector<K> get_keys(const map<K, V> &m)
 	return v;
 }
 
-vector<int> get_random_permutation(int n)
-{
-	vector<int> v;
-	for(int i = 0; i < n; i++) v.push_back(i);
-	for(int i = 0; i < n; i++)
-	{
-		int k = rand() % (n - i);
-		int x = v[k];
-		v[k] = v[n - i - 1];
-		v[n - i - 1] = x;
-	}
-	return v;
-}
+vector<int> get_random_permutation(int n);
 
 #endif
