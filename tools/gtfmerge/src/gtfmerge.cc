7ff07e3b9e1d67b23cc8072ad9b65c57d43a17c3
Mingfu Shao <shaomingfu@gmail.com>
2017-02-25 12:52:10 -0500

    revise intersection

--- tools/gtfmerge/src/gtfmerge.cc
+++ tools/gtfmerge/src/gtfmerge.cc
@@ -46,13 +46,17 @@ int gtfmerge::build_pairwise_intersection(genome1 &gm)
 	gm.clear();
 	for(int i = 0; i < genomes.size(); i++)
 	{
+		genome1 gx;
 		for(int j = i + 1; j < genomes.size(); j++)
 		{
 			genome1 g1;
 			genomes[i].build_intersection(genomes[j], g1);
-			gm.build_union(g1);
-			cout << "union the intersection between " << i << " and " << j << endl << flush;
+			gx.build_union(g1);
+			printf("union the intersection betwwen %d and %d: (%lu, %lu, %lu)\n", 
+					i, j, g1.transcripts.size(), gx.transcripts.size(), gm.transcripts.size());
+			fflush(stdout);
 		}
+		gm.build_union(gx);
 	}
 	return 0;
 }
