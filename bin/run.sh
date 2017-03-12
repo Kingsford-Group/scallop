636caed4fdf8ad1a029c8b0184f1e27c89790a39
Mingfu Shao <shaomingfu@gmail.com>
2017-03-12 13:58:02 -0400

    add scripts

--- bin/run.sh
+++ bin/run.sh
@@ -1,23 +1,61 @@
 #!/bin/bash
 
-tag=B614
-abd=0.01
+if [ "$#" != "2" ]
+then
+	echo "run.sh tag abd"
+	exit
+fi
+
+tag=$1
+abd=$2
+
+#list="SRR534307.hisat"
+list="GSM981256.tophat \
+	  GSM981244.tophat \
+	  GSM984609.tophat\
+	  SRR307911.tophat \
+	  SRR387661.tophat \
+	  GSM981256.star \
+	  GSM981244.star \
+	  GSM984609.star \
+	  SRR307911.star \
+	  SRR387661.star \
+	  GSM981256.hisat \
+	  GSM981244.hisat \
+	  GSM984609.hisat \
+	  SRR307911.hisat \
+	  SRR387661.hisat"
 
 list="GSM981256.tophat \
-	  GSM981256.star \
-	  GSM981256.hisat \
 	  GSM981244.tophat \
-	  GSM981244.star \
-	  GSM981244.hisat \
 	  GSM984609.tophat\
-	  GSM984609.star \
-	  GSM984609.hisat \
 	  SRR307911.tophat \
-	  SRR307911.star \
-	  SRR307911.hisat \
 	  SRR387661.tophat \
+	  GSM981256.star \
+	  GSM981244.star \
+	  GSM984609.star \
+	  SRR307911.star \
 	  SRR387661.star \
-	  SRR387661.hisat"
+	  GSM981256.hisat \
+	  GSM981244.hisat \
+	  GSM984609.hisat \
+	  SRR307911.hisat \
+	  SRR387661.hisat \
+	  SRR545723.tophat \
+	  SRR315323.tophat \
+	  SRR307903.tophat \
+	  SRR315334.tophat \
+	  SRR534307.tophat \
+	  SRR545723.star \
+	  SRR315323.star \
+	  SRR307903.star \
+	  SRR315334.star \
+	  SRR534307.star \
+	  SRR545723.hisat \
+	  SRR315323.hisat \
+	  SRR307903.hisat \
+	  SRR315334.hisat \
+	  SRR534307.hisat"
 
 for x in `echo $list`
 do
--- bin/summary.roc.sh
+++ bin/summary.roc.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [ "$#" != 1 ]; then
-	echo "usage ./summary.roc.sh tag"
+if [ "$#" != 2 ]; then
+	echo "usage ./summary.roc.sh tag abd"
 	exit
 fi
 
 tag=$1
+abd=$2
 roc="mul"
 
-scallop="scallop"."$1"."0.01"
-stringtie="stringtie.1.3.2d.0.01"
-transcomb="transcomb.0.01"
+
+scallop="scallop"."$1"."$abd"
+stringtie="stringtie.1.3.2d"."$abd"
+transcomb="transcomb"."$abd"
 
 list="GSM981256.tophat \
 	  GSM981256.star \
@@ -27,6 +29,39 @@ list="GSM981256.tophat \
 	  SRR387661.tophat \
 	  SRR387661.star \
 	  SRR387661.hisat"
+
+
+list="GSM981256.tophat \
+	  GSM981244.tophat \
+	  GSM984609.tophat\
+	  SRR307911.tophat \
+	  SRR387661.tophat \
+	  GSM981256.star \
+	  GSM981244.star \
+	  GSM984609.star \
+	  SRR307911.star \
+	  SRR387661.star \
+	  GSM981256.hisat \
+	  GSM981244.hisat \
+	  GSM984609.hisat \
+	  SRR307911.hisat \
+	  SRR387661.hisat \
+	  SRR545723.tophat \
+	  SRR315323.tophat \
+	  SRR307903.tophat \
+	  SRR315334.tophat \
+	  SRR534307.tophat \
+	  SRR545723.star \
+	  SRR315323.star \
+	  SRR307903.star \
+	  SRR315334.star \
+	  SRR534307.star \
+	  SRR545723.hisat \
+	  SRR315323.hisat \
+	  SRR307903.hisat \
+	  SRR315334.hisat \
+	  SRR534307.hisat"
+
 
 
 for i in `echo $list`
