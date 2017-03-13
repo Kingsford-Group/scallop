142a5e3a3883d9ab1983dc7688eb5b7c40b62f98
Mingfu Shao <shaomingfu@gmail.com>
2017-03-13 15:58:28 -0400

    update scripts

--- bin/run.venn.single.sh
+++ bin/run.venn.single.sh
@@ -6,17 +6,25 @@ then
 	exit
 fi
 
-sc="scallop.B505.0.01"
+sc="scallop.B668.0.01"
 st="stringtie.1.3.2d.0.01"
 tc="transcomb.0.01"
 
-dir=`pwd`/$1.W505.1.3.2d
-gtf=`pwd`/p2_sorted.gtf
-
-roc=`pwd`/gtfcuff
-commgtf=`pwd`/common.gtf.sh
-
+dir=`pwd`/$1.W668.1.3.2d
 mkdir -p $dir
+
+gtf=`pwd`/p2_sorted.gtf
+roc=`pwd`/gtfcuff
+gtfmerge=`pwd`/gtfmerge
+
+function commgtf()
+{
+	tmp=./list.xxx.tmp;
+	echo $1 > $tmp;
+	echo $2 >> $tmp;
+	$gtfmerge intersection $tmp $3;
+	rm -rf $tmp;
+}
 
 ln -sf `pwd`/$1.tophat/$sc/scallop.gtf $dir/scallop.1.gtf
 ln -sf `pwd`/$1.star/$sc/scallop.gtf $dir/scallop.2.gtf
@@ -66,10 +74,10 @@ a3="transcomb"
 xx=$1
 for k in `echo "1 2"`
 do
-	$commgtf $dir/$a1.$k.true.gtf $dir/$a2.$k.true.gtf > $dir/$a1.$a2.$k.true.gtf
-	$commgtf $dir/$a1.$k.true.gtf $dir/$a3.$k.true.gtf > $dir/$a1.$a3.$k.true.gtf
-	$commgtf $dir/$a2.$k.true.gtf $dir/$a3.$k.true.gtf > $dir/$a2.$a3.$k.true.gtf
-	$commgtf $dir/$a1.$a2.$k.true.gtf $dir/$a3.$k.true.gtf > $dir/$a1.$a2.$a3.$k.true.gtf
+	commgtf $dir/$a1.$k.true.gtf $dir/$a2.$k.true.gtf  $dir/$a1.$a2.$k.true.gtf
+	commgtf $dir/$a1.$k.true.gtf $dir/$a3.$k.true.gtf  $dir/$a1.$a3.$k.true.gtf
+	commgtf $dir/$a2.$k.true.gtf $dir/$a3.$k.true.gtf  $dir/$a2.$a3.$k.true.gtf
+	commgtf $dir/$a1.$a2.$k.true.gtf $dir/$a3.$k.true.gtf  $dir/$a1.$a2.$a3.$k.true.gtf
 
 	xa=`cat $dir/$a1.$k.true.gtf | awk '$3 == "transcript"' | wc -l`
 	xb=`cat $dir/$a2.$k.true.gtf | awk '$3 == "transcript"' | wc -l`
@@ -83,7 +91,7 @@ done
 
 for k in `echo "3"`
 do
-	$commgtf $dir/$a1.$k.true.gtf $dir/$a2.$k.true.gtf > $dir/$a1.$a2.$k.true.gtf
+	commgtf $dir/$a1.$k.true.gtf $dir/$a2.$k.true.gtf  $dir/$a1.$a2.$k.true.gtf
 	xa=`cat $dir/$a1.$k.true.gtf | awk '$3 == "transcript"' | wc -l`
 	xb=`cat $dir/$a2.$k.true.gtf | awk '$3 == "transcript"' | wc -l`
 	x1=`cat $dir/$a1.$a2.$k.true.gtf | awk '$3 == "transcript"' | wc -l`
@@ -94,17 +102,17 @@ echo $xx > $dir/algo.summary
 
 # different aligner
 cd $dir
-$commgtf $dir/scallop.1.true.gtf $dir/scallop.2.true.gtf > $dir/scallop.12.true.gtf
-$commgtf $dir/scallop.1.true.gtf $dir/scallop.3.true.gtf > $dir/scallop.13.true.gtf
-$commgtf $dir/scallop.2.true.gtf $dir/scallop.3.true.gtf > $dir/scallop.23.true.gtf
-$commgtf $dir/scallop.1.true.gtf $dir/scallop.23.true.gtf > $dir/scallop.123.true.gtf
+commgtf $dir/scallop.1.true.gtf $dir/scallop.2.true.gtf  $dir/scallop.12.true.gtf
+commgtf $dir/scallop.1.true.gtf $dir/scallop.3.true.gtf $dir/scallop.13.true.gtf
+commgtf $dir/scallop.2.true.gtf $dir/scallop.3.true.gtf $dir/scallop.23.true.gtf
+commgtf $dir/scallop.1.true.gtf $dir/scallop.23.true.gtf $dir/scallop.123.true.gtf
 
-$commgtf $dir/stringtie.1.true.gtf $dir/stringtie.2.true.gtf > $dir/stringtie.12.true.gtf
-$commgtf $dir/stringtie.1.true.gtf $dir/stringtie.3.true.gtf > $dir/stringtie.13.true.gtf
-$commgtf $dir/stringtie.2.true.gtf $dir/stringtie.3.true.gtf > $dir/stringtie.23.true.gtf
-$commgtf $dir/stringtie.1.true.gtf $dir/stringtie.23.true.gtf > $dir/stringtie.123.true.gtf
+commgtf $dir/stringtie.1.true.gtf $dir/stringtie.2.true.gtf $dir/stringtie.12.true.gtf
+commgtf $dir/stringtie.1.true.gtf $dir/stringtie.3.true.gtf $dir/stringtie.13.true.gtf
+commgtf $dir/stringtie.2.true.gtf $dir/stringtie.3.true.gtf $dir/stringtie.23.true.gtf
+commgtf $dir/stringtie.1.true.gtf $dir/stringtie.23.true.gtf $dir/stringtie.123.true.gtf
 
-$commgtf $dir/transcomb.1.true.gtf $dir/transcomb.2.true.gtf > $dir/transcomb.12.true.gtf
+commgtf $dir/transcomb.1.true.gtf $dir/transcomb.2.true.gtf $dir/transcomb.12.true.gtf
 
 xx=$1
 for k in `echo "1 2 3 12 13 23 123"`
