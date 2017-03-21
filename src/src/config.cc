869da2a67f87b9e653715d11c639bf3cb1eb9eea
Mingfu Shao <shaomingfu@gmail.com>
2017-03-20 22:40:28 -0400

    update version, prepare for v0.9.6

--- src/src/config.cc
+++ src/src/config.cc
@@ -65,7 +65,7 @@ bool output_tex_files = false;
 string fixed_gene_name = "";
 int batch_bundle_size = 100;
 int verbose = 1;
-string version = "v0.9.5";
+string version = "v0.9.6";
 
 int parse_arguments(int argc, const char ** argv)
 {
