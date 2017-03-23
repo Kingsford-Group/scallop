4a60208023c2e2d2b26080e3fc4f6a95020c5fe0
Mingfu Shao <shaomingfu@gmail.com>
2017-03-23 16:55:20 -0400

    update scripts

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-tag="B668"
+tag=""
+#./run.sh $tag 0.01
 
-./run.sh $tag 0.01
-
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 1
 
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 2.5
 
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 5
 
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 7.5
 
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 10
 
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 25
 
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 50
 
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 75
 
-sleep 1.11
+sleep 0.00
 ./run.sh $tag 100
--- bin/run.scallop.sh
+++ bin/run.scallop.sh
@@ -10,17 +10,16 @@ dir=`pwd`/$1/scallop.$3
 mkdir -p $dir
 
 bam=`pwd`/$1/$2/"$2".sort.bam
-gtf=`pwd`/$1/expression.gtf
+gtf=/home/mingfus/data/repositories/scallop/bin/gencode.v25.gtf
+#gtf=`pwd`/$1/expression.gtf
 
-{ /usr/bin/time -v ./scallop -i $bam -o $dir/scallop.gtf $4 > $dir/scallop.log; } 2> $dir/time.log
-
-mv $dir/scallop.gtf $dir/scallop0.gtf
-./gtfformat format $dir/scallop0.gtf $dir/scallop.gtf
-
-if [ "$2" == "hisat" ]; then
-	cat $dir/scallop.gtf | sed 's/^/chr/g' > $dir/scallop.tmp.xxx.gtf
-	mv $dir/scallop.tmp.xxx.gtf $dir/scallop.gtf
-fi
+#{ /usr/bin/time -v ./scallop -i $bam -o $dir/scallop.gtf $4 > $dir/scallop.log; } 2> $dir/time.log
+#mv $dir/scallop.gtf $dir/scallop0.gtf
+#./gtfformat format $dir/scallop0.gtf $dir/scallop.gtf
+#if [ "$2" == "hisat" ]; then
+#	cat $dir/scallop.gtf | sed 's/^/chr/g' > $dir/scallop.tmp.xxx.gtf
+#	mv $dir/scallop.tmp.xxx.gtf $dir/scallop.gtf
+#fi
 
 cd $dir
 gffcompare -o gffmul -r $gtf $dir/scallop.gtf -M -N
