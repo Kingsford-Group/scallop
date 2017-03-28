c9e86bae10aa2c73a97aaa5ff0cd57d3fed35a70
Mingfu Shao <shaomingfu@gmail.com>
2017-03-28 17:58:47 -0400

    rm scripts

--- bin/kill.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in `seq 1 100`
-do
-	killall stringtie scallop gtfcompare gffcompare
-	sleep 1
-done
--- bin/run.venn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in `cat list10`
-do
-	echo $i
-	nohup ./run.venn.single.sh $i &
-done
